ea620801a1eb480eb53251ede9a859bbc7b2cc22
Fabio Silva <fabio.s0@hotmail.com>
2022-07-21 18:29:00 -0300

    Adicionando timeStamp a entidade Restaurante

--- Api/src/main/java/com/github/fabioscp0/domain/model/Restaurante.java
+++ Api/src/main/java/com/github/fabioscp0/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.github.fabioscp0.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -43,6 +47,14 @@ public class Restaurante {
 	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
+	
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime dataCadastro;
+	
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime dataAtualizacao;
 	
 	@JsonIgnore
 	@ManyToMany
--- Api/src/main/resources/import.sql
+++ Api/src/main/resources/import.sql
@@ -14,8 +14,8 @@ insert into forma_pagamento (descricao) values ('Dinheiro')
 insert into permissao (nome,descricao) values ('Alteracao', 'Altera Usuario')
 
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Burguer King', 10, 1, utc_timestamp, utc_timestamp, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
-insert into restaurante (nome,taxa_frete,cozinha_id) values ('Mc Donalds','7.50',1)
-insert into restaurante (nome,taxa_frete,cozinha_id) values ('Girafas','7.50',2)
-insert into restaurante (nome,taxa_frete,cozinha_id) values ('Subway','0.0',2)
+insert into restaurante (nome,taxa_frete,cozinha_id, data_cadastro, data_atualizacao) values ('Mc Donalds','7.50',1, utc_timestamp, utc_timestamp)
+insert into restaurante (nome,taxa_frete,cozinha_id, data_cadastro, data_atualizacao) values ('Girafas','7.50',2, utc_timestamp, utc_timestamp)
+insert into restaurante (nome,taxa_frete,cozinha_id, data_cadastro, data_atualizacao) values ('Subway','0.0',2, utc_timestamp, utc_timestamp)
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1), (1,2), (1,3), (2,3), (3,2), (3,3)
